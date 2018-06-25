5c4e83af901162c2348cbb12314a1c1475a332ae
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-06-25 15:46:40 -0700

    Fix production release checks.

--- tests/confirm-rpm.sh
+++ tests/confirm-rpm.sh
@@ -48,7 +48,7 @@ then
 fi
 
 release=$(rpm -qp --queryformat '%{RELEASE}\n' "$pkgDir/$rpmName")
-if [ -z "$(echo "$release" | grep '^\([0-9]\{12\}\)\(\.git[0-9a-f]\{7\}\|[0-9]\{10\}|.dev|\)$')" ]
+if [ -z "$(echo "$release" | grep '^\([0-9]\{12\}\)$')" ] && [ -z "$(echo "$release" | grep '^\([0-9]\{12\}\)\.git[0-9a-f]\{7\}\|[0-9]\{10\}\)$')" ]
 then
   echo "Release $release does not match our expected format"
   exit 1
